664520e2a43551e58d0ccdf2ff1710c7c2f8a273
WShadowStudios <129547469+WShadowStudios@users.noreply.github.com>
2025-02-02 19:07:39 +0300

    Update GoldenTabletFPLYS_EDITION.lua

--- GoldenTabletFPLYS_EDITION.lua
+++ GoldenTabletFPLYS_EDITION.lua
@@ -35,7 +35,7 @@ surfacelight.Enabled = false
 local showflv = false
 
 local LastScannedRoom = -1
-local CalculatedFPSWait = (5)
+local CalculatedFPSWait = (1)
 
 local IsScannerOpened = false
 local Equipped = false
